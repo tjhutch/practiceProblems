6981633457bb3d6642d7ec9f58bbf2b6d8b202f2
Taylor Hutchinson <Taylor.Hutchinson@macys.com>
2018-06-04 09:13:34 -0700

    O(n) solution for LRU cache)

--- problems/LRUCache.js
+++ problems/LRUCache.js
@@ -1,19 +1,21 @@
-const cache = [];
+let cache;
 let capacity;
 /**
  * @param {number} cap
  */
-var LRUCache = function(cap) {
+const LRUCache = function(cap) {
   capacity = cap;
+  cache = new Map()
 };
 
 /**
  * @param {number} key
  * @return {number}
  */
-LRUCache.prototype.get = function(key) {
-  if (cache[key]) {
-    return cache[key];
+/*LRUCache.prototype.*/const get = function(key) {
+  if (cache.has(key)) {
+    updateLRU(key);
+    return cache.get(key).value;
   } else {
     return -1;
   }
@@ -24,8 +26,24 @@ LRUCache.prototype.get = function(key) {
  * @param {number} value
  * @return {void}
  */
-LRUCache.prototype.put = function(key, value) {
-
+/*LRUCache.prototype. */const put = function(key, value) {
+  updateLRU(key);
+  cache.set(key, {age: 0, value});
+  if (cache.size > capacity) {
+    let oldest = 0;
+    let key;
+    for(let [k, val] of cache) {
+      if (val.age > oldest) {
+        oldest = val.age;
+        key = k;
+      }
+    }
+    if (key) {
+      cache.delete(key);
+    } else {
+      console.log('key to delete was null');
+    }
+  }
 };
 
 /**
@@ -34,3 +52,26 @@ LRUCache.prototype.put = function(key, value) {
  * var param_1 = obj.get(key)
  * obj.put(key,value)
  */
+const updateLRU = function(key) {
+  for(let [k, val] of cache) {
+    if (k === key) {
+      val.age = 0;
+    } else {
+      val.age += 1;
+    }
+  }
+};
+
+let result;
+LRUCache(2);
+get(2);
+put(2, 1);
+put(1, 1);
+result = get(2);
+put(4, 1);
+result = get(1);
+result = get(2);
+put(4, 4);
+result = get(1);
+result = get(3);
+result = get(4);
